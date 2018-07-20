cbb272a7323bd770d35a23402a8474b18c5a9972
Yufei Wang <bigyuffie@gmail.com>
2018-07-20 14:53:31 -0400

    First test

--- test1.py
+++ test1.py
@@ -1,3 +1,10 @@
-import zipline
+from zipline.api import order, record, symbol
 
-#This is a test
+
+def initialize(context):
+    pass
+
+
+def handle_data(context, data):
+    order(symbol('AAPL'), 10)
+    record(AAPL=data.current(symbol('AAPL'), 'price'))
